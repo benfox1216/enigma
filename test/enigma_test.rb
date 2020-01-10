d841c10855bdf6be6dfe2a63d12859f689c5fbad
Ben Fox <benfox1216@gmail.com>
2020-01-10 07:14:29 -0700

    Set up rake/simplecov, add non-working suggested methods & tests

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,5 +1,4 @@
-require 'minitest/autorun'
-require 'minitest/pride'
+require_relative 'test_helper'
 require './lib/enigma'
 
 class EnigmaTest < Minitest::Test
@@ -9,6 +8,10 @@ class EnigmaTest < Minitest::Test
   
   def test_it_exists
     assert_instance_of Enigma, @enigma
+  end
+  
+  def test_it_has_a_date
+    assert_equal "date", @enigma.today
   end
   
   def test_it_can_encrypt
@@ -21,5 +24,17 @@ class EnigmaTest < Minitest::Test
     expected = {decryption: "hello world", key: "02715", date: "040895"}
     
     assert_equal expected, @enigma.decrypt("keder ohulw", "02715", "040895")
+  end
+  
+  def test_it_has_rotate_method
+    assert_equal "array", @enigma.rotate
+  end
+  
+  def test_it_can_generate_date
+    assert_equal "date", @enigma.strftime
+  end
+  
+  def test_it_has_with_index_method
+    assert_equal "enumerator", @enigma.with_index
   end
 end
