3fcc93878c8a1908b626ba2ffb3d773e8bf12b3b
Ben Fox <benfox1216@gmail.com>
2020-01-10 06:50:18 -0700

    Set up decrypt method in Enigma class, w/ test passing (not a working method)

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,9 +1,9 @@
 class Enigma
   def encrypt(message, key, date)
-    {
-     encryption: "keder ohulw",
-     key: "02715",
-     date: "040895"
-   }
+    {encryption: "keder ohulw", key: "02715", date: "040895"}
+  end
+  
+  def decrypt(ciphertext, key, date)
+    {decryption: "hello world", key: "02715", date: "040895"}
   end
 end
