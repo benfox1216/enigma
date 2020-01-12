c0bce342ed1d5648e8400cf73311a3496f425032
Ben Fox <benfox1216@gmail.com>
2020-01-12 14:33:33 -0700

    Add code so special characters are translated as themselves, all tests passing (100% coverage)

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,4 +1,5 @@
 require './lib/encryption'
+require './lib/decryption'
 
 class Enigma
   def encrypt(message, key, date)
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -19,4 +19,24 @@ class EnigmaTest < Minitest::Test
     expected = {decryption: "hello world", key: "02715", date: "040895"}
     assert_equal expected, @enigma.decrypt("keder ohulw", "02715", "040895")
   end
+  
+  def test_it_can_encrypt_messages_with_uppercase_letters
+    expected = {encryption: "keder ohulw", key: "02715", date: "040895"}
+    assert_equal expected, @enigma.encrypt("Hello World", "02715", "040895")
+  end
+  
+  def test_it_can_decrypt_messages_with_uppercase_letters
+    expected = {decryption: "hello world", key: "02715", date: "040895"}
+    assert_equal expected, @enigma.decrypt("Keder Ohulw", "02715", "040895")
+  end
+  
+  def test_it_can_encrypt_messages_with_special_characters
+    expected = {encryption: "keder ohulw!", key: "02715", date: "040895"}
+    assert_equal expected, @enigma.encrypt("hello world!", "02715", "040895")
+  end
+
+  def test_it_can_decrypt_messages_with_special_letters
+    expected = {decryption: "hello world!", key: "02715", date: "040895"}
+    assert_equal expected, @enigma.decrypt("Keder Ohulw!", "02715", "040895")
+  end
 end
